7b1d0a1a9334d84fcc0f7d7d73ade52299b1036c
Hemapriya Selvaraj <hemapriyaaselvaraj@gmail.com>
2026-01-31 09:48:20 +0530

    Time: 18 ms (56.76%), Space: 60.1 MB (30.68%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -4,9 +4,22 @@
  * @return {boolean}
  */
 var canConstruct = function(ransomNote, magazine) {
-      let arr = ransomNote.split();
-      return  magazine.includes(arr);
+     let obj = {};
+     for(let val of magazine){
+        if(obj[val]){
+            obj[val]++;
+        }else{
+            obj[val] = 1;
+        }
+     }
+
+     for(let val of ransomNote){
+        if(!obj[val]  || obj[val]<0){
+            return false;
+        }else{
+            obj[val]--;
+        }
+     }
     
-        
-    
+    return true;
 };
