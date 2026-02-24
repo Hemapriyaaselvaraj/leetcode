35744628f79a2d466b18cb35e6a35833fefa6048
Hemapriya Selvaraj <hemapriyaaselvaraj@gmail.com>
2026-02-24 18:25:36 +0530

    Time: 7 ms (12.09%), Space: 56.6 MB (21.98%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -4,13 +4,14 @@
  */
 var countEven = function(num) {
     let count = 0;
-    let sum = 0;
+    let sum;
     let val;
     let arr = [];
     for(let i=1;i<=num;i++){
         arr.push(i);
     }
     for(let number of arr){
+        sum = 0;
         value = number.toString().split("");
         for(let val of value){
            sum+=Number(val);
