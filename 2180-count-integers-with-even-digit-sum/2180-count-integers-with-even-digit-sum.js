0709ec5344ce11f3e94e306554807828fc5e6da9
Hemapriya Selvaraj <hemapriyaaselvaraj@gmail.com>
2026-02-24 15:49:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var countEven = function(num) {
+    let count = 0;
+    let sum = 0;
+    let val;
+    let arr = [];
+    for(let i=1;i<=num;i++){
+        arr.push(i);
+    }
+    for(let number of arr){
+        value = number.toString().split("");
+        for(let val of value){
+           sum+=Number(val);
+        }
+        if(sum%2 === 0){
+            count++;
+        }
+    }
+    return count;
+};
