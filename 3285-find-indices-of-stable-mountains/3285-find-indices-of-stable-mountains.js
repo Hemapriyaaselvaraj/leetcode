f4f763491ee7c2991787fbe44e9057ce2537e257
Hemapriya Selvaraj <hemapriyaaselvaraj@gmail.com>
2026-04-02 23:08:29 +0530

    Time: 1 ms (43.1%), Space: 57.1 MB (64.66%) - LeetHub

---
+++ 3285-find-indices-of-stable-mountains/3285-find-indices-of-stable-mountains.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} height
+ * @param {number} threshold
+ * @return {number[]}
+ */
+var stableMountains = function(height, threshold) {
+    let res = [];
+    for(let i=0;i<height.length;i++){
+        if(height[i-1]>threshold){
+            res.push(i);
+        }
+    }
+    return res;
+};
